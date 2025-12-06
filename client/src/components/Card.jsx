31be4536e4c20b38191562a16d6b70bf87b5cb75
theH4Sh <syedhaseebnaqvi51214@gmail.com>
2025-12-06 14:32:38 +0500

    card fixed

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,12 +1,13 @@
-const Card = ({ image, name, price }) => {
+const Card = ({ image, name, description, price }) => {
     return ( 
-        <div className="p-4 rounded-lg shadow-sm h-72 w-96 bg-slate cursor-pointer hover:shadow-xl transition-shadow duration-300">
+        <div className="p-4 rounded-lg shadow-sm h-[350px] w-96 bg-slate cursor-pointer hover:shadow-xl transition-shadow duration-300">
             <div className="hover:blur-xs hover:transition hover:duration-300">
                 <img src={`http://localhost:8000/images/${image}`} alt={name} className="w-full h-auto rounded-md mb-4"/>
             </div>
             <div className="flex flex-col items-start">
-                <h2 className="text-lg font-bold">{name}</h2>
-                <p className="text-gray-500 text-sm">${price}</p>
+                <h2 className="text-lg font-bold py-0.5">{name}</h2>
+                <p className="text-gray-500">{description}</p>
+                <p className="text-lg font-semibold pt-5">${price} <span className="text-gray-500 text-xs">PKR</span></p>
             </div>
         </div>
      );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,23 +1,26 @@
 import Card from "../components/Card";
 import { useFetch } from "../hooks/useFetch";
+import { Link } from "react-router-dom";
 
 const Home = () => {
     const url = "http://localhost:8000/api/product";
     const { data, loading, error } = useFetch(url);
 
     return ( 
-        <div>
+        <div className="flex flex-col items-center">
             <h1 className="text-2xl font-bold mb-6">Product List</h1>
             {loading && <p>Loading...</p>}
             {error && <p>Error: {error.message}</p>}
-            <div className="grid grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {data && data.map((product) => (
-                    <Card 
-                        key={product._id}
-                        image={product.image}
-                        name={product.name}
-                        price={product.price}
-                    />
+                    <Link to={`/product/${product._id}`} key={product._id}>
+                        <Card
+                            image={product.image}
+                            name={product.name}
+                            description={product.description}
+                            price={product.price}
+                        />
+                    </Link>
                 ))}
             </div>
         </div>
