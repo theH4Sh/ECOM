4d008a2440e4f1ccbeb29bb6a45e49e082a4c475
theH4Sh <syedhaseebnaqvi51214@gmail.com>
2025-12-07 16:19:12 +0500

    route protection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route, Navigate } from 'react-router-dom'
 import RootLayout from './layout/RootLayout'
 import Home from './pages/Home.jsx'
 import { Toaster } from 'react-hot-toast'
@@ -7,17 +7,20 @@ import Login from './pages/Login.jsx'
 import SignUp from './pages/SignUp.jsx'
 import Product from './pages/Product.jsx'
 import OrderHistory from './pages/OrderHistory.jsx'
+import { useSelector } from 'react-redux'
 
 function App() {
+
+  const auth = useSelector((state) => state.auth);
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
         <Route index element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
+        <Route path="login" element={ !auth.isAuthenticated ? <Login /> : <Navigate to='/' />} />
+        <Route path="signup" element={ !auth.isAuthenticated ? <SignUp /> : <Navigate to='/' />} />
         <Route path="product/:id" element={<Product />} />
-        <Route path="orders" element={<OrderHistory />} />
+        <Route path="orders" element={ auth.isAuthenticated ? <OrderHistory /> : <Navigate to='/' />} />
       </Route>
     )
   )
