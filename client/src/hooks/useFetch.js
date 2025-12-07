96d3e72a81773995524ac6a5ed275088763c1ee6
theH4Sh <syedhaseebnaqvi51214@gmail.com>
2025-12-07 15:58:11 +0500

    hook improved

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 export const useFetch = (url) => {
 
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+
+    const { token } = useSelector((state) => state.auth);
     
     useEffect(() => {
         const fetchData = async () => {
@@ -14,6 +17,7 @@ export const useFetch = (url) => {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
                     },
                 });
                 
