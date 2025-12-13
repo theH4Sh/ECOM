5b19016dde7257f92eac0265db39d2ec2e16c49b
theH4Sh <syedhaseebnaqvi51214@gmail.com>
2025-12-13 23:39:47 +0500

    improved response

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -2,18 +2,46 @@ const Product = require('../models/Product')
 const mongoose = require('mongoose')
 
 const getAllProducts = async (req, res, next) => {
-    try {
-        const page = req.query.page || 1
-        const limit = req.query.limit || 10
-        const skip = (page - 1) * limit
+  try {
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
 
-        const products = await Product.find().skip(skip).limit(limit)
+    const products = await Product.aggregate([
+      {
+        $lookup: {
+          from: "reviews",               // Mongo collection name
+          localField: "_id",
+          foreignField: "product",
+          as: "reviews"
+        }
+      },
+      {
+        $addFields: {
+          reviewCount: { $size: "$reviews" },
+          averageRating: {
+            $cond: [
+              { $gt: [{ $size: "$reviews" }, 0] },
+              { $avg: "$reviews.rating" },
+              0
+            ]
+          }
+        }
+      },
+      {
+        $project: {
+          reviews: 0 // remove heavy array
+        }
+      },
+      { $skip: skip },
+      { $limit: limit }
+    ]);
 
-        res.json(products)
-    } catch (error) {
-        next(error)
-    }
-}
+    res.status(200).json(products);
+  } catch (error) {
+    next(error);
+  }
+};
 
 const getProduct = async (req, res, next) => {
     try {
