2b9a42d3ed86b0cb855f46088315e279e71bc46c
theH4Sh <syedhaseebnaqvi51214@gmail.com>
2026-01-25 22:22:47 +0500

    top scroller

--- client/src/layout/RootLayout.jsx
+++ client/src/layout/RootLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 const RootLayout = () => {
     return ( 
         <div>
+            <ScrollToTop />
             <nav>
                 <Navbar/>
             </nav>
